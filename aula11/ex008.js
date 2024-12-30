b15d831bf976377fd0ea4c8717f20efff7f1f1aa
Willian Silva <willvrsilva@gmail.com>
2024-12-30 08:52:11 -0300

    aulas e exercicios gustavo guanabara

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+} else{
+    console.log('Parabéns você está na velocidade correta da via!')
+}
---
+++ aula12/011.js
@@ -0,0 +1,9 @@
+var idade = 27
+console.log(`Você tem ${idade} anos.`)
+    if(idade < 16){
+    console.log('Você não pode votar.')
+} else if(idade >= 16 && idade < 18 || idade >= 60)
+    console.log('Voto opcional.')
+    else
+    console.log('Voto obrigatório')
+    
