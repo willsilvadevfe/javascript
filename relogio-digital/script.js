0e4a1c752747bdf5889936cdeba3f7e1c59356e5
Willian Silva <willvrsilva@gmail.com>
2025-01-27 09:11:29 -0300

    anotações

--- relogio-digital/script.js
+++ relogio-digital/script.js
@@ -1,24 +1,27 @@
-let horas = document.getElementById('hour')
-let minutes = document.getElementById('minutes')
-let seconds = document.getElementById('seconds')
+let horas = document.getElementById('hour') //Cria variavel com elemento ID
+let minutes = document.getElementById('minutes') //Cria variavel com elemento ID
+let seconds = document.getElementById('seconds') //Cria variavel com elemento ID
 
 
-let relogio = setInterval(function time(){
-    let dateToday = new Date()
-    let hr = dateToday.getHours()
-    let min = dateToday.getMinutes()
-    let sec = dateToday.getSeconds()
+let relogio = setInterval(function time(){ //Cria uma function para relógio com setInterval
+    let dateToday = new Date() //Variavel para data exata, puxa info hr, min e sec
+    let hr = dateToday.getHours() //Define variavel para pegar hora exata
+    let min = dateToday.getMinutes() //Define variavel para pegar minuto exat
+    let sec = dateToday.getSeconds() //Define variavel para pegar segundos exatos
     
 
-    if(hr < 10) hr = '0' + hr
-    if(min < 10) min = '0' + min
-    if(sec < 10) sec = '0' + sec
+    if(hr < 10) hr = '0' + hr //Se as horas forem menor que 10 add um '0' antes, sempre 2 digitos.
+    if(min < 10) min = '0' + min //Se os minutos forem menor que 10 add um '0' antes, sempre 2 digitos.
+    if(sec < 10) sec = '0' + sec //Se os segundos forem menor que 10 add um '0' antes, sempre 2 digitos.
 
-    horas.textContent = hr
-    minutes.textContent = min
-    seconds.textContent = sec
+    horas.textContent = hr //Pega a variavel horas(ID) e add hora exata na tela.
+    minutes.textContent = min //Pega a variavel minutos(ID) e add hora exata na tela.
+    seconds.textContent = sec //Pega a variavel segundos(ID) e add hora exata na tela.
 })
 
+
+//Formatação de estilo (CSS) para as variaveis horas, minutes e seconds.
+//Melhorar...
 horas.style.color = 'orange'
 horas.style.fontSize = '6.2em'
 horas.style.fontFamily = 'Orbitron'
