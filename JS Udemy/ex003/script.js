ace7d06e82a524d9a1b7214d94caa6b612304e12
Willian Silva <willvrsilva@gmail.com>
2025-02-02 22:59:45 -0300

    ex003

---
+++ JS Udemy/ex003/script.js
@@ -0,0 +1,18 @@
+let num = 10 //Váriavel com valor atribuido 10
+num += 5 //Váriavel somando valor 5 atribuido
+num *= 5  //Váriavel multiplicando valor 5 atribuido
+num += 1 //Váriavel somando valor 1 atribuido
+
+window.alert('O resto da divisão de ' + num + ' por 6 é: ' + num%6) //Mostra na tela a váriavel num e o resto da divisão da variavel por 6
+
+window.alert('O quadrado do resto da divisão de ' + num + ' por 6 é: ' + (num%6)**2) //Mostra na tela a váriavel num e o resto da divisão da variavel por 6 elevado ao quadrado
+
+window.alert('O valor da variavel é: ' + num) //Mostra o valor da variavel
+
+num = num / 4 //O valor da variavel divido por 4
+
+window.alert('O valor da variavel dividido por 4 é: ' + num) //Mostra na tela o valor da variavel dividido por 4
+
+window.alert('O valor final da variavel é: ' + num) //Mostra na tela o valor final da variavel
+
+
