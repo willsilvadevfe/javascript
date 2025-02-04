430123aa390fed56e2e94f70d97bb03b9d9ccf75
Willian Silva <willvrsilva@gmail.com>
2025-02-04 13:44:49 -0300

    ex004
    boolean

---
+++ JS Udemy/ex004/script.js
@@ -0,0 +1,14 @@
+/*let n1 = parseFloat(prompt('Digite um número de 0 à 10:'))
+let n2 = parseFloat(prompt('Digite outro número de 0 à 10:'))
+alert('O segundo número é maior do que o primeiro? ' + (n2 >= n1))*/
+
+//Exercicio aula (final)
+
+let n1 = parseFloat(prompt('Digite um número de 0 à 10'))
+let n2 = parseFloat(prompt('Digite outro número de 0 à 10'))
+
+soma = n1 + n2
+media = soma / 2
+
+alert('A média entre os valores digitados é maior que 7 ?   ' + (media > 7))
+
