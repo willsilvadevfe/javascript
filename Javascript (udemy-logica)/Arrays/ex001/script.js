f22c257790f9bdf5f449bf96452e0ccad918b5e3
Willian Silva <willvrsilva@gmail.com>
2025-03-04 12:43:45 -0300

    arrays

---
+++ Javascript (udemy-logica)/Arrays/ex001/script.js
@@ -0,0 +1,3 @@
+let nomes = ['João', 'Mathias', 'Tamires', 'Jonas', 'Patricia', 'Osmar', 'Jurandir', 'Carla', 'Maria']
+console.log(nomes)
+console.log(nomes.length)
--- Javascript (udemy-logica)/ex016/script.js
+++ Javascript (udemy-logica)/ex016/script.js
@@ -1,1 +1,14 @@
 /* Mostre na tela todos os anos bissextos segundo a regra do mundo real (procure na internet como saber se o ano é bissexto ou não). Utilize os controladores de fluxo BREAK e/ou CONTINUE caso queira (opcional) */
+
+for(let ano = 1004; ano <= 2024 ; ano+=4){
+    let anoBissexto = ano % 100 != 0 || ano % 400 === 0
+
+    if(anoBissexto){
+    console.log(`${ano} Ano bissexto detectado...`)
+}else{
+    console.log(`${ano} Não é bissexto!`)
+}}
+
+
+
+
