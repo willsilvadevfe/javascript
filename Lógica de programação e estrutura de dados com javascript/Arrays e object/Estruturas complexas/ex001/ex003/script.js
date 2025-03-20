142ccf7a54dcef75bc18510ba86b53acc39e6434
Willian Silva <willvrsilva@gmail.com>
2025-03-20 11:06:02 -0300

    ex003

--- Lógica de programação e estrutura de dados com javascript/Arrays e object/Estruturas complexas/ex001/ex003/script.js
+++ Lógica de programação e estrutura de dados com javascript/Arrays e object/Estruturas complexas/ex001/ex003/script.js
@@ -4,11 +4,17 @@ const elementos = [
         {nome: "teclado", preco: 50, quantidade: 42},
         {nome: "mouse", preco: 35, quantidade: 22},
         {nome: "monitor", preco: 450, quantidade: 18},
+        {nome: "gabinete", preco: 233, quantidade: 10},
+        {nome: "headset", preco: 90, quantidade: 12},
 ]
 
-for(let i = 0; i <= elementos.length; i++){
+for(let i = 0; i < elementos.length; i++){
     const list = document.getElementById('list')
-    list.innerHTML += `<td>${elementos[i].nome}</td><td>${elementos[i].preco}</td><td>${elementos[i].quantidade}</td>`
+    list.innerHTML += `<tr>
+                <td>${elementos[i].nome}</td>
+                <td>${elementos[i].preco.toFixed(2)}</td>
+                <td>${elementos[i].quantidade}</td>
+            </tr>`
     
 }
 
