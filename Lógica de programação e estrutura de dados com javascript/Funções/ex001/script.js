61488cf9862223120bec91c905fae7c557350666
Willian Silva <willvrsilva@gmail.com>
2025-03-28 13:48:43 -0300

    mobile first tabuada

--- Lógica de programação e estrutura de dados com javascript/Funções/ex001/script.js
+++ Lógica de programação e estrutura de dados com javascript/Funções/ex001/script.js
@@ -3,12 +3,17 @@ function clicar(){
     const btn = document.getElementById('btn')
     const boxValue = document.getElementById('numberbox')
 
+    tab.style.color = '#fff'
+    tab.style.textAlign = 'center'
+    tab.style.marginTop = '50px'
+    tab.style.fontSize = '1.5rem'
+
     if(boxValue.value <= 0 || boxValue.value > 100 || boxValue.value == isNaN(boxValue)){
         alert('Digite apenas números entre 1 e 100 para gerar sua tabuada, números negativos, números iguais a 0 e qualquer outro tipo de caracter não é válido.')
     }else{
         let contador = 0
-        while(contador <= 20){
-            tab.innerHTML += `${boxValue.value} X ${contador} = ${boxValue.value * contador} </br>`
+        while(contador <= 10){
+            tab.innerHTML += `${boxValue.value} X ${contador} = <strong>${boxValue.value * contador}</strong> </br>`
             contador++
           
         }
