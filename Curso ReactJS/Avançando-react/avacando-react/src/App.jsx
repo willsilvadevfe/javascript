be1d382765542c83987e139ebc93edcf19e2a9e8
Willian Silva <willvrsilva@gmail.com>
2026-05-06 08:39:22 -0300

    Importando imagens
    public & assets

---
+++ Curso ReactJS/Avançando-react/avacando-react/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Road from './assets/road.jpg'
+
+function App() {
+
+  return (
+    <>
+    <h2>Avançando em React</h2>
+    <p>Importando imagem da pasta public <code>src="/house.jpg" alt="House"</code></p>
+    <img src="/house.jpg" alt="House" />
+
+    <p>Importando imagem da pasta assets</p>
+    <p>Importar a imagem no inicio do código como se fosse um componente <code>import Road from './assets/road.jpg'</code></p>
+    <p>chamar via tag <strong>img</strong> no código, passando o <strong>src</strong> entre chaves com o <strong>nome do componente.</strong></p>
+    <img src={Road} alt="Road" />
+
+    </>
+  )
+}
+
+export default App
