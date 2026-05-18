579f223f6eac6b13bd631a7c0128968e3726c951
Willian Silva <willvrsilva@gmail.com>
2026-05-18 11:04:40 -0300

    CSS Inline, components

--- Curso ReactJS/CSS React/css-react/src/App.jsx
+++ Curso ReactJS/CSS React/css-react/src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import MyComponent from './components/MyComponent'
+import { useState } from 'react'
 
 function App() {
- 
-
+ const status = true
+ const [name] = useState("Willian")
   return (
     <>
       <h2>Iniciando CSS com React</h2>
+      <MyComponent />
+      {/* INLINE CSS */}
+      <p style={{color: "white", background: "gray", padding: "6px"}}>Este elemento foi estilizado em forma inline</p>
+      {/* INLINE Style Dinâmico */}
+      <h2 style={status === true ? ({color: "green", background: "lightgreen"}) : ({color: "red", background: "lightgray"})}>CSS Estilo dinâmico Inline</h2>
+      <h3 style={name === "Willian" ? ({color: "red"}) : null}>Willian</h3>
     </>
   )
 }
---
+++ Curso ReactJS/CSS React/css-react/src/components/MyComponent.jsx
@@ -0,0 +1,12 @@
+import './MyComponent.css'
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h2>MyComponent CSS (Componentes)</h2>
+        <p className='my-comp-paragraph'>Esse paragráfo está no <code>App.jsx</code></p>
+    </div>
+  )
+}
+
+export default MyComponent
