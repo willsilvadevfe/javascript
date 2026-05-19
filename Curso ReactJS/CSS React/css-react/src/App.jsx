775bca6746836d2e0c493931e5186bf20c8c2d39
Willian Silva <willvrsilva@gmail.com>
2026-05-19 09:09:19 -0300

    css dinamica e modules

--- Curso ReactJS/CSS React/css-react/src/App.jsx
+++ Curso ReactJS/CSS React/css-react/src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
 import MyComponent from './components/MyComponent'
 import { useState } from 'react'
+import MyTitle from './components/MyTitle'
 
 function App() {
+ const classeDinamica = false
  const status = true
  const [name] = useState("Willian")
   return (
@@ -12,8 +14,14 @@ function App() {
       {/* INLINE CSS */}
       <p style={{color: "white", background: "gray", padding: "6px"}}>Este elemento foi estilizado em forma inline</p>
       {/* INLINE Style Dinâmico */}
-      <h2 style={status === true ? ({color: "green", background: "lightgreen"}) : ({color: "red", background: "lightgray"})}>CSS Estilo dinâmico Inline</h2>
+      <h2 style={status ? ({color: "green", background: "lightgreen"}) : ({color: "red", background: "lightgray"})}>CSS Estilo dinâmico Inline</h2>
       <h3 style={name === "Willian" ? ({color: "red"}) : null}>Willian</h3>
+      {/* Classe dinâmica */}
+      <div className={classeDinamica ? 'classTrue' : 'classFalse'}></div>
+      {/* CSS Modules */}
+      <MyTitle />
+      
+
     </>
   )
 }
---
+++ Curso ReactJS/CSS React/css-react/src/components/MyTitle.jsx
@@ -0,0 +1,11 @@
+import styles from './MyTitle.module.css'
+
+const MyTitle = () => {
+  return (
+    <div>
+        <h1 className={styles.my_title}>CSS Modules</h1>
+    </div>
+  )
+}
+
+export default MyTitle
