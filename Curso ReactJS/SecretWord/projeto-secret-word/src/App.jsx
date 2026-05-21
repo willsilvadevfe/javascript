1886c2450c126aaf36ead2394d58d637f3c724a3
Willian Silva <willvrsilva@gmail.com>
2026-05-21 09:38:34 -0300

    Secret Word
    CSS + Inicio js

--- Curso ReactJS/SecretWord/projeto-secret-word/src/App.jsx
+++ Curso ReactJS/SecretWord/projeto-secret-word/src/App.jsx
@@ -1,13 +1,35 @@
-import './App.css'
+//React
+import { useCallback, useEffect, useState } from "react";
+
+//CSS
+import "./App.css";
+
+//Data
+import { wordsList } from "./data/words";
+
+//Components
+import StartScreen from "./components/StartScreen";
+import GameOver from "./components/GameOver";
+import Game from "./components/Game";
+
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"}
+]
 
 function App() {
-  
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
 
+  console.log(words)
   return (
     <>
-      <h2>Secret Word</h2>
+    {gameStage === 'start' && <StartScreen />}
+    {gameStage === 'game' && <Game />}
+    {gameStage === 'end' && <GameOver />} 
     </>
-  )
+  );
 }
 
-export default App
+export default App;
