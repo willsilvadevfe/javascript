e8180643c86a698a205747487ebd03bfd3486af0
Willian Silva <willvrsilva@gmail.com>
2026-05-22 09:15:36 -0300

    navegação
    navegação entre páginas do projeto

--- Curso ReactJS/SecretWord/projeto-secret-word/src/components/GameOver.jsx
+++ Curso ReactJS/SecretWord/projeto-secret-word/src/components/GameOver.jsx
@@ -1,8 +1,11 @@
 import './GameOver.css'
 
-const GameOver = () => {
+const GameOver = ({retry}) => {
   return (
-    <div>GameOver</div>
+    <div>
+      <h1>Fim do jogo</h1>
+      <button onClick={retry}>Reiniciar Jogo</button>
+    </div>
   )
 }
 
