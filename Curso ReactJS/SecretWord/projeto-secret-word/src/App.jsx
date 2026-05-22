ef9373756ae7ed3b12971a8b35093f14d3f6f711
Willian Silva <willvrsilva@gmail.com>
2026-05-22 11:16:14 -0300

    funções e estados

--- Curso ReactJS/SecretWord/projeto-secret-word/src/App.jsx
+++ Curso ReactJS/SecretWord/projeto-secret-word/src/App.jsx
@@ -13,31 +13,62 @@ import GameOver from "./components/GameOver";
 import Game from "./components/Game";
 
 const stages = [
-  {id: 1, name: "start"},
-  {id: 2, name: "game"},
-  {id: 3, name: "end"}
-]
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 function App() {
-  const [gameStage, setGameStage] = useState(stages[0].name)
-  const [words] = useState(wordsList)
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    //Pick a random category
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+    console.log(category);
+
+    //Pick a random word
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    console.log(word);
+    return { word, category };
+  };
 
   const startGame = () => {
-  setGameStage(stages[1].name)
-}
+    const { word, category } = pickWordAndCategory();
+    let wordLetters = word.split("");
 
- const verifyLetter = () => {
-  setGameStage(stages[2].name)
- }
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
 
- const retry = () => {
-  setGameStage(stages[0].name)
- }
+    console.log(word, category);
+    console.log(wordLetters);
+
+    //Fill states
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
+    setGameStage(stages[1].name);
+  };
+
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
   return (
     <>
-    {gameStage === 'start' && <StartScreen startGame={startGame}/>}
-    {gameStage === 'game' && <Game  verifyLetter={verifyLetter}/>}
-    {gameStage === 'end' && <GameOver retry={retry}/>} 
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry} />}
     </>
   );
 }
