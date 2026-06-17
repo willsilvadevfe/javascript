b0423fc2aaf48662f8a241553342badb937d8aa0
Willian Silva <willvrsilva@gmail.com>
2026-06-17 11:19:15 -0300

    att

---
+++ despachante/projeto/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "./components/Despachante.jsx";
+import CrisDespachante from "./components/Despachante.jsx";
+
+function App() {
+  return (
+    <>
+      <CrisDespachante />
+    </>
+  );
+}
+
+export default App;
